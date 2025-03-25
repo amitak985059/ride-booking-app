47d7f85716fc7f8c72c6eade4a4b432e825356e8
Amit <amitak985059@gmail.com>
2025-03-26 01:06:06 +0530

    tmc

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+
+app.use(cors);
+
+app.get('/', (req, res) =>{
+    res.send('hello works')
+})
+
+
+module.exports = app;
