2e486e94543a7b55e44599f9ea8a58fd7c101ce5
Amit <amitak985059@gmail.com>
2025-03-29 01:11:19 +0530

    userLoginReactPageDevelped

--- frontend/src/pages/UserLogin.jsx
+++ frontend/src/pages/UserLogin.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {Link} from 'react-router-dom'
+
+
+
 
 const UserLogin = () => {
+
+const [email, setEmail] = useState('')
+const [password, setPassword] = useState('')
+const [userData, setuserData] = useState({})
+
+const submitHandler = (e)=>{
+  e.preventDefault();
+  setuserData({
+    email:email,
+    password:password
+  })
+  setEmail('')
+  setPassword('')
+  console.log(userData)
+
+}
+
   return (
-    <div>UserLogin</div>
-  )
+    <>
+      <div className='p-7'>
+        <div>
+          <form onSubmit={(e)=>{
+            submitHandler(e)
+          }} action="">
+            <h3 className='text-xl mb-2'>What's you email</h3>
+            <input
+              type="email"
+              className='bg-[#73bdbb] mb-7rounded px-4 py-4 w-full text-lg placeholder:text-base border'
+              required
+              placeholder='Your email'
+              value={email}
+              onChange={(e)=>{
+                setEmail(e.target.value)
+              }}
+            />
+            <h3 className='text-xl mb-2'>Enter Password </h3>
+            <input
+              type="password"
+              className='bg-[#73bdbb] mb-7rounded px-4 py-4 w-full text-lg placeholder:text-base border'
+              required
+              placeholder='your password'
+              value={password}
+              onChange={(e)=>{
+                setPassword(e.target.value)
+              }}
+               />
+            <button className='bg-[#111] text-white font-semibold mb-7rounded px-4 py-4 w-full text-lg placeholder:text-base'>Login </button>
+            <Link to = '/signup'className='text-blue-600'>Create new account</Link>
+          </form>
+        </div>
+        <div>
+          <Link to='/captain-login' className='pt-4 flex items-centre justify-centre mt-4 bg-[#288222] text-white font-semibold mb-7rounded px-4 py-4 w-full text-lg placeholder:text-base'>Sign in as Captain</Link>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default UserLogin
