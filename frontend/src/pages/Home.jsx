37f302557d918dd6785687550b51781fce1d1795
Amit <amitak985059@gmail.com>
2025-06-06 01:13:09 +0530

    updatad gsap and added the smoothing functionality also smothened it in video thie was near 4

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,6 @@
-import React, { useRef } from 'react'
-import {useGSAP} from '@gsap/react'
+import React, { useRef, useState } from 'react';
+import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
-import { useState } from 'react';
 
 const Home = () => {
   const [pickup, setPickup] = useState('');
@@ -10,42 +9,85 @@ const Home = () => {
   const panelRef = useRef(null);
   const submitHandler = (e) => {
     e.preventDefault();
-  }
+  };
 
   useGSAP(() => {
-    if(panelOpen){
+    if (panelOpen) {
       gsap.to(panelRef.current, {
         height: '70%',
-      })
-    }else{
+        duration: 0.5,
+        ease: 'power2.out',
+      });
+    } else {
       gsap.to(panelRef.current, {
         height: '0%',
-      })
+        duration: 0.5,
+        ease: 'power2.inOut',
+      });
     }
   }, [panelOpen]);
+
   return (
     <div>
-      <img className='w-16 h-10 absolute left-5 top-5' src="https://upload.wikimedia.org/wikipedia/commons/5/58/Uber_logo_2018.svg" alt="" />
+      {/* Logo */}
+      <img
+        className="w-16 h-10 absolute left-5 top-5"
+        src="https://upload.wikimedia.org/wikipedia/commons/5/58/Uber_logo_2018.svg"
+        alt=""
+      />
+
+      {/* Background Image */}
       <div>
-        <img className='w-screen object-cover h-screen' src="https://images.unsplash.com/photo-1650217124806-36e7a0b7afb8?q=80&w=3027&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Uber ride animation" />
+        <img
+          className="w-screen object-cover h-screen"
+          src="https://images.unsplash.com/photo-1650217124806-36e7a0b7afb8?q=80&w=3027&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="Uber ride animation"
+        />
       </div>
 
-      <div className='absolute display flex flex-col justify-end text-color-white h-screen top-0'>
-        <div className='h-[30%] bg-white p-5'>
-          <h4 className='text-2xl font-bold mt-5 ml-5'>
+      {/* Form and Expanding Panel */}
+      <div className="absolute flex flex-col justify-end text-white h-screen top-0 left-0 w-full">
+        {/* Static Form */}
+        <div className="h-[30%] bg-white p-5">
+          <h4 className="text-2xl font-bold mt-5 ml-5 text-black">
             Find the right Ride for you
           </h4>
-          <form onSubmit={(e) => { submitHandler(e) }} className='bg-white w-full p-2' action="" type="text">
-            <input onClick={() => { setPanelOpen(true) }} value={pickup} onChange={(e) => { setPickup(e.target.value) }} className='bg-[#eee] px-8 py-2 text-lg rounded-lg mt-6 w-full' type="text" placeholder='Where to?' />
-            <input onClick={() => { setPanelOpen(true) }} value={destination} onChange={(e) => { setDestination(e.target.value) }} className='bg-[#eee] px-8 py-2 text-lg rounded-lg mt-4 w-full' type="text" placeholder='Destination' />
+          <form onSubmit={submitHandler} className="bg-white w-full p-2">
+            <input
+              onClick={() => setPanelOpen(prev => !prev)}
+              value={pickup}
+              onChange={(e) => setPickup(e.target.value)}
+              className="bg-[#eee] px-8 py-2 text-lg rounded-lg mt-6 w-full placeholder:text-gray-500"
+              type="text"
+              placeholder="Where to?"
+            />
+
+            <input
+              onClick={() => setPanelOpen(prev => !prev)}
+              value={destination}
+              onChange={(e) => setDestination(e.target.value)}
+              className="bg-[#eee] px-8 py-2 text-lg rounded-lg mt-4 w-full placeholder:text-gray-500"
+              type="text"
+              placeholder="Destination"
+            />
+
           </form>
         </div>
-        <div ref={panelRef} className='h-[70%] bg-red p-5 hidden'>
-
+        {/* Expanding Panel */}
+        <div
+          ref={panelRef}
+          className="bg-white w-full overflow-hidden"
+          style={{ height: '0%' }}
+        >
+          {/* Content of expanding panel */}
+          <div className="p-5 text-black">
+            <h2 className="text-xl font-semibold">Suggested Locations</h2>
+            {/* Add more content here */}
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
